4086860db5161db5a01e9f082135310b11435c91
Chloe Fabro <156484403+Chloemh48@users.noreply.github.com>
2024-02-06 23:28:18 -0800

    created run() function
    In wp2.py, created run (). This function will handle and execute different operations based on user input, particularly, encrypty and decrypt the file

--- wp2.py
+++ wp2.py
@@ -1,1 +1,34 @@
+import sys 
+from pathlib import Path
+import decrypt_
+import encrypt_
+
+
+def run():
+    user_input =input ("Enter your command:")
+
+    if len(sys.argv)!= 4:
+        print("Usage: python wp2.py -e|-d <inputfile> <outputfile> <key>")
+        raise SystemExit(1)
+    
+    command = sys.argv[1]
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
+    key = sys.argv[4]
+
+    if command == 'e':
+        encrypt_.encrypt(input_file, output_file, key)
+    elif command == 'd':
+        decrypt_.decrypt(input_file, output_file, key)
+    else:
+        print('Invalid Cammand')
+
+if __name__ == "__main__":
+   run()
+
+
+
+   
+    
+
 
